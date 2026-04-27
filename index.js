e9a9b30f31040f10f9ba43dd6aedf479994393f9
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-04-27 05:33:54 +0100

    express.js installed, gitignore file added... basic setup done...

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Ecobazaar ecommerce');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('app created...')
+});
+
+app.listen(5000, () => {
+    console.log('Server Connected to port 5000 ...');
+    
+});
+
+
